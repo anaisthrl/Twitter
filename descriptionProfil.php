a8034ead3c19a8b770350a369b883d645c2ef815
anaisthrl <50917972+anaisthrl@users.noreply.github.com>
2019-06-13 00:05:47 +0200

    CSS

---
+++ descriptionProfil.php
@@ -0,0 +1,18 @@
+<!--description du profil-->
+
+
+
+
+
+<aside>
+
+    <h6><?php echo $params['user']['pseudo'] ?></h6>
+
+    <h7>@<?php echo $params['user']['user_name'] ?></h7>
+
+    <h7><?php echo $params['user']['info_perso'] ?></h7>
+
+    <h7>Né le <?php echo $params['user']['birth'] ?></h7>
+    <h8>Se déconnecter</h8>
+
+</aside>
